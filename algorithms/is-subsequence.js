3a906fa6318ee93a6ee7b315d8eff05c8b61914d
jzolo22 <jzolotarev@gmail.com>
2021-02-11 17:10:42 -0500

    maxSubarraySum completed

--- algorithms/is-subsequence.js
+++ algorithms/is-subsequence.js
@@ -45,3 +45,12 @@ console.log(isSubsequence('abc', 'acb'))
 console.log(isSubsequence('abc', 'abracadabra'))
 
 
+// RECURSIVE SOLUTION
+// function isSubsequence(str1, str2) {
+//     if(str1.length === 0) return true
+//     if(str2.length === 0) return false
+//     if(str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))  
+//     return isSubsequence(str1, str2.slice(1))
+// }
+
+
---
+++ algorithms/max-subarray-sum.js
@@ -0,0 +1,51 @@
+/*
+
+Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
+
+**The subarray must consist of consecutive elements from the original array
+
+Input: an array, a number (of elements)
+Output: number (max sum of consecutive elements)
+
+Potential Questions/Edge Cases:
+    -Empty array? 
+    -Array has fewer elements than length int ---> return null
+
+Time Complexity: O(N)
+Space Complexity: O(1)
+
+*/
+
+
+
+function maxSubarraySum(arr, num) {
+    // if array.length is less than the num, return null
+    if (arr.length < num) return null
+
+    // initialize a tempsum to 0
+    let tempSum = 0
+    // initialize a maxsum to 0
+    let maxSum = 0
+
+    // loop through arr just for the first elements up to the amount specified by num
+    for (let i = 0; i < num; i++){
+        // add them to tempsum
+        tempSum += arr[i]
+    }
+
+    // set maxsum = tempsum
+    maxSum = tempSum
+
+    // set up another loop 
+    for (let j = num; j < arr.length; j++) {
+        // subtract the first part of the window
+        // add the last part of the window
+        tempSum = tempSum - arr[j - num] + arr[j]
+        // if the tempsum is larger than the maxsum, set maxsum to tempsum
+        maxSum = Math.max(tempSum, maxSum)
+    }
+
+    return maxSum
+}
+
+console.log(maxSubarraySum([100,200,300,400], 2))
