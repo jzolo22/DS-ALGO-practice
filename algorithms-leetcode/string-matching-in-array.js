b761a51b9ab83b5c6d995438355d812d694e338c
jzolo22 <jzolotarev@gmail.com>
2021-02-15 19:48:22 -0500

    added string matching problem

---
+++ algorithms-leetcode/string-matching-in-array.js
@@ -0,0 +1,14 @@
+/*
+
+Given an array of string words. Return all strings in words which is substring of another word in any order. 
+
+String words[i] is substring of words[j], if can be obtained removing some characters to left and/or right side of words[j].
+
+Input: words = ["mass","as","hero","superhero"]
+Output: ["as","hero"]
+Explanation: "as" is substring of "mass" and "hero" is substring of "superhero".
+["hero","as"] is also a valid answer.
+
+*/
+
+function stringMatching
