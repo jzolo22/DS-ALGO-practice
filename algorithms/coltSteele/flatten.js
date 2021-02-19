95486bd80519f4aeff1d88a1855d1df9e3cd2965
jzolo22 <jzolotarev@gmail.com>
2021-02-19 18:34:26 -0500

    flatten completed

--- algorithms/coltSteele/flatten.js
+++ algorithms/coltSteele/flatten.js
@@ -4,6 +4,22 @@ Write a recursive function called flatten which accepts an array of arrays and r
 
 */
 
-function flatten() {
-    
+// base case: reach the end of array
+// create new array which is empty
+// iterate over full array, if element is !array, push it into new array
+// if it's an array, call function again
+
+function flatten(arr) {
+    if (!arr.length) return []
+    let newArr = []
+    for (const element in arr){
+        if (typeof arr[element] !== "object"){
+            newArr.push(arr[element])
+        } else {
+            newArr = newArr.concat(flatten(arr[element]))
+        }
+    }
+    return newArr
 }
+
+console.log(flatten([1, [2, [3, 4], [[5]]]]))
