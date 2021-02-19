b0b4eca3f6201def1d7055529a432bd8881da2c3
jzolo22 <jzolotarev@gmail.com>
2021-02-19 18:44:14 -0500

    stringifyNumbers added

---
+++ algorithms/coltSteele/stringifyNumbers.js
@@ -0,0 +1,5 @@
+/*
+
+Write a function called stringifyNumbers which takes in an object and finds all the values which are numbers and converts them to strings. Recursion would be a great way to solve this. 
+
+*/
