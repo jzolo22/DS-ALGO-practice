c1968a2ec91db4d7d2fa4d408d43d08ce9998550
jzolo22 <jzolotarev@gmail.com>
2021-02-19 19:00:34 -0500

    my solution to stringifyNumbers using JS6 spread operator

--- algorithms/coltSteele/stringifyNumbers.js
+++ algorithms/coltSteele/stringifyNumbers.js
@@ -3,3 +3,35 @@
 Write a function called stringifyNumbers which takes in an object and finds all the values which are numbers and converts them to strings. Recursion would be a great way to solve this. 
 
 */
+
+let obj = {
+    num: 1,
+    test: [],
+    data: {
+        val: 4,
+        info: {
+            isRight: true,
+            random: 66
+        }
+    }
+}
+
+// base case: iterated through all of keys 
+// use a for...in loop
+// if obj[key] is a number
+//  then obj[key] = obj[key].toString()
+// if it's an obj, call function again on that obj
+
+function stringifyNumbers(obj) {
+    let newObj = {...obj}
+    for (const key in obj){
+        if (typeof obj[key] === "number"){
+            newObj[key] = obj[key].toString()
+        } else if (typeof obj[key] === "object"){
+            newObj[key] = stringifyNumbers(obj[key])
+        }
+    }
+    return newObj
+}
+
+console.log(stringifyNumbers(obj))
