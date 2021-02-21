26826e4497649f765074244678eacbd65b8a3d63
jzolo22 <jzolotarev@gmail.com>
2021-02-20 19:56:21 -0500

    first attempt at collectStrings

---
+++ algorithms/coltSteele/collectStrings.js
@@ -0,0 +1,43 @@
+/*
+
+Write a function called collectStrings which accepts an object and returns an array of the all the values in the object that have a type of string
+
+*/
+
+// base case: iterate through whole object
+//
+// create an empty array
+// if type is string, push it into the empty array
+// otherwise, concatenate with return of collectString
+
+const obj = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+}
+
+function collectStrings(obj) {
+    let stringArray = []
+
+    for (const key in obj) {
+        if (typeof obj[key] === "string"){
+            stringArray.push(obj[key])
+        } else {
+            stringArray.concat(collectStrings(obj[key]))
+        }
+    }
+
+    return stringArray
+}
+
+console.log(collectStrings(obj))
