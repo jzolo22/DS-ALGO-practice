c43cf9501342ecd8933a2f12b69a12137217523b
jzolo22 <jzolotarev@gmail.com>
2021-02-20 19:42:32 -0500

    fixed stringifyNumbers

--- algorithms/coltSteele/stringifyNumbers.js
+++ algorithms/coltSteele/stringifyNumbers.js
@@ -23,12 +23,14 @@ let obj = {
 // if it's an obj, call function again on that obj
 
 function stringifyNumbers(obj) {
-    let newObj = {...obj}
+    let newObj = {}
     for (const key in obj){
         if (typeof obj[key] === "number"){
             newObj[key] = obj[key].toString()
-        } else if (typeof obj[key] === "object"){
+        } else if (typeof obj[key] === "object" && !Array.isArray(obj[key])){
             newObj[key] = stringifyNumbers(obj[key])
+        } else {
+            newObj[key] = obj[key]
         }
     }
     return newObj
