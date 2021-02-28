d1a2048520507b32e9a9dccf53d5581887396179
jzolo22 <jzolotarev@gmail.com>
2021-02-28 15:26:04 -0500

    add notes to bubbleSort

---
+++ algorithms/coltSteele/searchAlgorithms/bubbleSort/bubbleNotes.js
@@ -0,0 +1,9 @@
+/*
+
+A sorting algorithm where the largest values "bubble" up to the top.
+
+Compare two elements, swap from left to right if left one is larger.
+
+https://visualgo.net/en/sorting
+
+*/
