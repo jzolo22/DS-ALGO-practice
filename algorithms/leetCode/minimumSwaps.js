ce281fb3a5cd00c3d4e83536ab32536235a3c7f7
jzolo22 <jzolotarev@gmail.com>
2021-02-28 12:52:31 -0500

    minimumSwap added

---
+++ algorithms/leetCode/minimumSwaps.js
@@ -0,0 +1,17 @@
+/*
+
+You are given two strings s1 and s2 of equal length consisting of letters "x" and "y" only. Your task is to make these two strings equal to each other. You can swap any two characters that belong to different strings, which means: swap s1[i] and s2[j].
+
+Return the minimum number of swaps required to make s1 and s2 equal, or return -1 if it is impossible to do so.
+
+Input: s1 = "xx", s2 = "yy"
+Output: 1
+Explanation: 
+Swap s1[0] and s2[1], s1 = "yx", s2 = "yx"
+
+*/
+
+function minimumSwap(){
+    
+}
+
