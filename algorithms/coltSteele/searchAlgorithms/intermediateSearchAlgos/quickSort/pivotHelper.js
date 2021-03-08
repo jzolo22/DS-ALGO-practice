22e05ad8f7a6a5eb02ea06fd2fb22d83f69d1649
Julia Zolotarev <jzolotarev@gmail.com>
2021-03-08 17:31:57 -0500

    add pseudocode to pivotHelper

--- algorithms/coltSteele/searchAlgorithms/intermediateSearchAlgos/quickSort/pivotHelper.js
+++ algorithms/coltSteele/searchAlgorithms/intermediateSearchAlgos/quickSort/pivotHelper.js
@@ -17,6 +17,13 @@
 - Ideally the pivot should be chosen so that it's roughly the median value in the dataset you're sorting
 
 - For simplicity, we'll always select the first element (but this does have consequences)
-
-
  */
+
+//  PIVOT PSEUDOCODE
+// It will help to accept 3 arguments: array, start index, end index (can default to 0 and array.length -1)
+// Grab the pivot from the start of the array
+// Store the current pivot index in a variable (this will keep track of where the pivot should end up)
+// Loop through the array from start to end
+    // if the pivot is greater than the current element, incrememnt the pivot index variable and then swap the current element with the element at the pivot index
+// Swap the starting element (i.e. the pivot) with the pivot index 
+// Return the pivot index
---
+++ algorithms/coltSteele/searchAlgorithms/intermediateSearchAlgos/quickSort/quickNotes.md
@@ -0,0 +1,4 @@
+## **Quick Sort**
+- like merge sort, takes advantage of the fact that arrays of 0 or 1 elements are always sorted
+- works by selecting one element called "the pivot" and finding the index where the pivot should end up in the sorted array
+- once the pivot is positioned appropriately, quick sort can be applied on either side of the pivot
