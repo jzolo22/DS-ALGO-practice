503867946be236ad78a62ef954666771161a43e5
Julia Zolotarev <jzolotarev@gmail.com>
2021-03-07 19:34:40 -0500

    update merge function solution

--- algorithms/coltSteele/searchAlgorithms/intermediateSearchAlgos/mergeSort/mergeNotes.js
+++ algorithms/coltSteele/searchAlgorithms/intermediateSearchAlgos/mergeSort/mergeNotes.js
@@ -23,23 +23,27 @@ function mergeArrays(arr1, arr2){
     let point1 = 0
     let point2 = 0
 
-    while (point1 < arr1.length || point2 < arr2.length){
-        if (point1 === arr1.length){
-            mergedArray.push(arr2[point2])
-            point2++
-        } else if (point2 === arr2.length){
+    while (point1 < arr1.length && point2 < arr2.length){
+        if (arr1[point1] <= arr2[point2]){
             mergedArray.push(arr1[point1])
             point1++
-       }
-        if (arr1[point1] < arr2[point2]){
-            mergedArray.push(arr1[point1])
-            point1++
-        } else if (arr2[point2] < arr1[point1]){
+        } else (arr2[point2] < arr1[point1]){
             mergedArray.push(arr2[point2])
             point2++
         }
     }
+
+    while (point1 < arr1.length){
+        mergedArray.push(arr1[point1])
+        point1++
+    } 
+    while (point2 < arr2.length){
+        mergedArray.push(arr2[point2])
+        point2++
+    } 
+    
     return mergedArray
 }
 
 console.log(mergeArrays([1,3,5,7], [2,4,6,8]))
+console.log(mergeArrays([1,10,50], [2,14,99,100]))
